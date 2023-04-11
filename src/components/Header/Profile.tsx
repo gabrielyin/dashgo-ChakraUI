2a855c46a300013e8a85547ba3c47f63c783206f
gabrielyin <2021gabrielyin@gmail.com>
2023-04-10 23:17:09 -0300

    responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,21 +1,29 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+
+
     return (
         <Flex
             align='center'
         >
-            <Box mr='4' textAlign='right'>
-                <Text>
-                    Gabriel Yin
-                </Text>
-                <Text
-                    color='gray.300'
-                    fontSize='small'
-                >
-                    2021gabrielyin@gmail.com
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr='4' textAlign='right'>
+                    <Text>
+                        Gabriel Yin
+                    </Text>
+                    <Text
+                        color='gray.300'
+                        fontSize='small'
+                    >
+                        2021gabrielyin@gmail.com
+                    </Text>
+                </Box>
+            )}
 
             <Avatar size='md' name='Gabriel Yin' src='https://github.com/gabrielyin.png' />
         </Flex>
